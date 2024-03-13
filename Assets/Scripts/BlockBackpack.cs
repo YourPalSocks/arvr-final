34222aa1d34f99d6a20729c88436ecee0817606b
YourPalSocks <ethanMH2002@gmail.com>
2024-03-13 17:05:03 -0400

    Resize blocks, backpack functionality

--- Assets/Scripts/BlockBackpack.cs
+++ Assets/Scripts/BlockBackpack.cs
@@ -7,25 +7,54 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BlockBackpack : MonoBehaviour
 {
-    public XRController rController;
-    public XRController lController;
+    public ActionBasedController rController;
+    public ActionBasedController lController;
 
-    private InputActionReference gripRef;
+    public List<GameObject> blocks;
 
     private Collider col;
 
+    private const float MAX_TIME = 50;
+    private float curTime = 0;
+
+
     // Start is called before the first frame update
     void Start()
-    {  
+    {
         col = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(col.bounds.Contains(rController.transform.position))
+        Debug.Log(curTime);
+
+        if (curTime <= 0)
         {
-            Debug.Log("Entered!");
+            if (col.bounds.Contains(rController.transform.position) && rController.selectAction.action.inProgress)
+            {
+                Debug.Log("WHAT");
+                SpawnAndGrab(rController);
+                curTime = MAX_TIME;
+            }
+
+            if (col.bounds.Contains(lController.transform.position) && lController.selectAction.action.inProgress)
+            {
+                SpawnAndGrab(lController);
+                curTime = MAX_TIME;
+            }
         }
+        else
+            curTime -= Time.deltaTime;
+    }
+
+    void SpawnAndGrab(ActionBasedController c)
+    {
+        // Randomly pick a block from blocks
+        int r = Random.Range(0, blocks.Count - 1);
+        // Spawn that block
+        GameObject spawned = Instantiate(blocks[r], c.transform.position, Quaternion.identity);
+        // Force hand to grab block
+        spawned.GetComponent<XRGrabInteractable>().interactionManager.ForceSelect(c.GetComponentInChildren<XRDirectInteractor>(), spawned.GetComponent<XRGrabInteractable>());
     }
 }
