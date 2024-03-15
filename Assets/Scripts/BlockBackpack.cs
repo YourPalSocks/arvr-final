f8d5d9f48e2f5dc6b5bf9c5136f462286fafb209
Ethan Heinlein <ethanMH2002@gmail.com>
2024-03-15 15:41:37 -0400

    Revert "Wrist panel for next block"
    This reverts commit 6f080c2624be427a55983e0b349990816f0c948a.

--- Assets/Scripts/BlockBackpack.cs
+++ Assets/Scripts/BlockBackpack.cs
@@ -11,28 +11,29 @@ public class BlockBackpack : MonoBehaviour
     public ActionBasedController lController;
 
     public List<GameObject> blocks;
-    private GameObject nextBlock;
 
     private Collider col;
 
-    private const float MAX_TIME = 5;
+    private const float MAX_TIME = 50;
     private float curTime = 0;
 
-    public HandPanel handPanel;
 
-
+    // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<Collider>();
-        SelectNextBlock();
     }
 
+    // Update is called once per frame
     void Update()
     {
+        Debug.Log(curTime);
+
         if (curTime <= 0)
         {
             if (col.bounds.Contains(rController.transform.position) && rController.selectAction.action.inProgress)
             {
+                Debug.Log("WHAT");
                 SpawnAndGrab(rController);
                 curTime = MAX_TIME;
             }
@@ -47,29 +48,13 @@ public class BlockBackpack : MonoBehaviour
             curTime -= Time.deltaTime;
     }
 
-    /// <summary>
-    /// Spawns the next block and forces the player to hold it.
-    /// </summary>
-    /// <param name="c">The controller initiating the interaction</param>
     void SpawnAndGrab(ActionBasedController c)
     {
+        // Randomly pick a block from blocks
+        int r = Random.Range(0, blocks.Count - 1);
         // Spawn that block
-        GameObject spawned = Instantiate(nextBlock, c.transform.position, Quaternion.identity);
+        GameObject spawned = Instantiate(blocks[r], c.transform.position, Quaternion.identity);
         // Force hand to grab block
         spawned.GetComponent<XRGrabInteractable>().interactionManager.ForceSelect(c.GetComponentInChildren<XRDirectInteractor>(), spawned.GetComponent<XRGrabInteractable>());
-        // Prep next block after spawning
-        SelectNextBlock();
-    }
-
-    /// <summary>
-    /// Selects a block from a list of blocks. Updates player gui.
-    /// </summary>
-    void SelectNextBlock()
-    {
-        // Select next block from block list
-        int r = Random.Range(0, blocks.Count - 1);
-        nextBlock = blocks[r];
-        // Update hand panel with block info
-        handPanel.UpdateHandPanel(nextBlock);
     }
 }
